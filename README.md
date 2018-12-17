136731a03899eb06ee2d34c29b3f592912e3b7db
Claud Rusnac <claudiu@rusnac.net>
2018-12-17 11:06:58 -0800

    Rewrote code to allow further langue adds.

--- README.md
+++ README.md
@@ -3,13 +3,14 @@ BIOY is a simple Romanian / English Wordpress Plugin that displays a daily readi
 
 # Usage
 
-### English
-`[bioy lang="en" translation="esv" col="3"]`
-
-### Romanian
-`[bioy lang="ro" translation="rmnn" col="3"]`
+`[bioy lang='en' translation='esv' col='3' locale='en_US' display_current_month='yes']`
 
 ### Optional Parameters
 
 - `translation` is used to specify which translation you would like to be dispyed.  See https://www.biblegateway.com/versions/ for avaialble translation
 - `col` is used to specify how many columns are dispalyed. If not specified, the default is 3 columns. 
+- `locale` is used to display the date in the proper language. If not specified, the default is based on your hosting provider. 
+- `display_current_month` is used to toggle the current Month/Year.  If not specified, the default is No
+
+### Adding a New lanauge
+This plugin can easilbe be extended to other lanagues by simply creating a new bioy_**langage**.xml file.  Simply copy an exsiting file, rename it to the language code that the file has been translated to, translate books (https://www.biblegateway.com/versions/New-King-James-Version-NKJV-Bible/#booklist) and reference the lanaguage in the shortcode.
--- bioy.php
+++ bioy.php
@@ -1,8 +1,8 @@
 <?php
 /*
 * Plugin Name: Bible in One Year
-* Description: Romanian / English Bible in One Year plugin. 
-* Version: 1.1
+* Description: Multi-Language Bible in One Year plugin. 
+* Version: 1.4
 * Author: Claud Rusnac
 * Author URI: http://www.srcreative.co
 */
@@ -11,19 +11,37 @@ define( 'bioy_path', plugin_dir_path( __FILE__ ));
 function bioy_files(){
         wp_enqueue_style( 'bioy', plugins_url('/css/bioy.css', __FILE__), false, '1.0.0', 'all');
     }
-    
 add_action('wp_enqueue_scripts', 'bioy_files');
 
- // Display the Bible in One year based on language. 
+    // Display the Bible in One year based on language. 
     function bbioy_create($atts){ ?>
 
         <?php if($atts[lang] == null): //Check Language.  If not set, show error ?>
-            
-            <div class="error">Please specify a language (lang='en' or lang='ro') in your shortcode.</div>
-        
+            <div class="error">Please specify a language (i.e. lang='en', lang='ro') in your shortcode.</div>
         <?php endif //End Check Language ?>
 
-        <?php if($atts[col] != null): //Col ?>
+        <?php $bioy_xml_file = 'bioy_'.$atts[lang].'.xml'; ?>
+
+        <?php if(!file_exists(bioy_path.'xml/'.$bioy_xml_file)): //Check Include File ?>
+            <div class="error">I cannot find the proper language XML file.  Please verify that your file is named correctly (<?php echo $bioy_xml_file; ?>).</div>
+        <?php else: ?>
+            <?php $bioy_xml = simplexml_load_file(bioy_path.'xml/'.$bioy_xml_file); ?>
+        <?php endif //End Check Include file ?>
+
+        <?php //Set Variables 
+            $current_day = date("j");
+            $current_month = date("n");
+            $biblegateway = 'http://www.biblegateway.com/passage/?search=';
+            $translation = $atts[translation];
+            $locale = $atts[locale];
+        ?>
+        
+        <?php if($atts[locale] != null): //Check if Locale is set and use it ?>
+            <?php setlocale(LC_ALL,$atts[locale]); ?>
+        <?php endif //End Check locale ?>
+        
+    
+        <?php if($atts[col] != null): //If Col is set, override CSS ?>
             <style>
               ul.bioy {
               columns: <?php echo $atts[col]; ?>;
@@ -32,68 +50,18 @@ add_action('wp_enqueue_scripts', 'bioy_files');
                 }
             </style>
         <?php endif //End Col ?>
-                        
-         <?php if($atts[lang] == "en"): //English Language ?>  
-         <?php setlocale(LC_ALL ,"en_EN"); date_default_timezone_set('America/Los_Angeles'); 
-
-            if (file_exists(bioy_path.'xml/360-reading-plan-en.xml')) {
-                $xml = simplexml_load_file(bioy_path.'xml/360-reading-plan-en.xml');
-            } else {
-                exit('Failed to open XML File.');
-            }
-                                 
-            //Set Variables
-            $current_day = date("j");
-            $current_month = date("n");
-            $biblegateway = 'http://www.biblegateway.com/passage/?search=';
-            $translation = $atts[translation];
-         ?>
         
-
-           <h2 class="bioy"><?php echo strftime("%B %Y"); ?></h2>
+            <?php if($atts[display_current_month] != 'no'): //Display Current Month Check ?>
+                <h2 class="bioy"><?php echo strftime("%B %Y"); ?></h2>
+            <?php endif //End Display Current Month Check ?>
     
             <ul class="bioy">
-                <?php foreach ($xml->children() as $verse): ?>
+                <?php foreach ($bioy_xml->children() as $verse): ?>
                     <?php if ($verse->month == $current_month): ?>
                          <li<?php if ($verse->day == $current_day): ?> class="current-day" <?php endif; ?>><span class="day-badge"><?php echo $verse->day; ?></span> <a href="<?php echo $biblegateway.$verse->verse; ?>&version=<?php echo $translation; ?>&interface=print" target="_blank"><?php echo $verse->verse; ?></a></li>
                     <?php endif; ?>
                 <?php endforeach; ?>    
             </ul>
-        
-         <?php endif //End English ?>
-         
-         
-         <?php if($atts[lang] == "ro"): //Romanian Language ?>
-         <?php setlocale(LC_ALL ,"ro_RO"); date_default_timezone_set('America/Los_Angeles'); 
-             
-            //Check for XML, if exsit Include XML File
-            if (file_exists(bioy_path.'xml/360-reading-plan-en.xml')) {
-                $xml = simplexml_load_file(bioy_path.'xml/360-reading-plan-ro.xml');
-
-            } else {
-                exit('Failed to open XML File.');
-            }
-                                 
-            //Set Variables
-            $current_day = date("j");
-            $current_month = date("n");
-            $biblegateway = 'http://www.biblegateway.com/passage/?search=';
-            $translation = $atts[translation];
-
-         ?>
-          <h2 class="bioy"><?php echo strftime("%B %Y"); ?></h2>
-          
-            <ul class="bioy">
-                <?php foreach ($xml->children() as $verse): ?>
-                    <?php if ($verse->month == $current_month): ?>
-                         <li<?php if ($verse->day == $current_day): ?> class="current-day" <?php endif; ?>><span class="day-badge"><?php echo $verse->day; ?></span> <a href="<?php echo $biblegateway.$verse->verse; ?>&version=<?php echo $translation; ?>&interface=print" target="_blank"><?php echo $verse->verse; ?></a></li>
-                    <?php endif; ?>
-                <?php endforeach; ?>    
-            </ul>
-                            
-            
-         <?php endif //End Romanian ?>
-    
     
     <?php }
     
