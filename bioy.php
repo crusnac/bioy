06762ff133778fbc0c535818b5347f8280de6306
Claud Rusnac <claudiu@rusnac.net>
2018-12-17 09:56:47 -0800

    Update bioy.php

--- bioy.php
+++ bioy.php
@@ -6,19 +6,18 @@
 * Author: Claud Rusnac
 * Author URI: http://www.srcreative.co
 */
-define( 'MY_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
+define( 'bioy_path', plugin_dir_path( __FILE__ ));
 
-function add_my_css_and_my_js_files(){
-        //wp_enqueue_script('your-script-name', $this->urlpath  . '/your-script-filename.js', array('jquery'), '1.2.3', true);
+function bioy_files(){
         wp_enqueue_style( 'bioy', plugins_url('/css/bioy.css', __FILE__), false, '1.0.0', 'all');
     }
     
-add_action('wp_enqueue_scripts', "add_my_css_and_my_js_files");
+add_action('wp_enqueue_scripts', 'bioy_files');
 
  // Display the Bible in One year based on language. 
     function bbioy_create($atts){ ?>
 
-        <?php if($atts[lang] == null): //Check Language ?>
+        <?php if($atts[lang] == null): //Check Language.  If not set, show error ?>
             
             <div class="error">Please specify a language (lang='en' or lang='ro') in your shortcode.</div>
         
@@ -26,28 +25,19 @@ add_action('wp_enqueue_scripts', "add_my_css_and_my_js_files");
 
         <?php if($atts[col] != null): //Col ?>
             <style>
-                ul.bioy {
+              ul.bioy {
               columns: <?php echo $atts[col]; ?>;
               -webkit-columns: <?php echo $atts[col]; ?>;
               -moz-columns: <?php echo $atts[col]; ?>;
                 }
             </style>
-            <?php else: ?>
-            <style>
-                ul.bioy {
-                  columns: 3;
-                  -webkit-columns: 3;
-                  -moz-columns: 3;
-                }
-            </style>
-
         <?php endif //End Col ?>
                         
          <?php if($atts[lang] == "en"): //English Language ?>  
          <?php setlocale(LC_ALL ,"en_EN"); date_default_timezone_set('America/Los_Angeles'); 
 
-            if (file_exists(MY_PLUGIN_PATH.'xml/360-reading-plan-en.xml')) {
-                $xml = simplexml_load_file(MY_PLUGIN_PATH.'xml/360-reading-plan-en.xml');
+            if (file_exists(bioy_path.'xml/360-reading-plan-en.xml')) {
+                $xml = simplexml_load_file(bioy_path.'xml/360-reading-plan-en.xml');
             } else {
                 exit('Failed to open XML File.');
             }
@@ -57,10 +47,10 @@ add_action('wp_enqueue_scripts', "add_my_css_and_my_js_files");
             $current_month = date("n");
             $biblegateway = 'http://www.biblegateway.com/passage/?search=';
             $translation = $atts[translation];
-        ?>
+         ?>
         
 
-         	<h2 class="bioy"><?php echo strftime("%B %Y"); ?></h2>
+           <h2 class="bioy"><?php echo strftime("%B %Y"); ?></h2>
     
             <ul class="bioy">
                 <?php foreach ($xml->children() as $verse): ?>
@@ -75,9 +65,10 @@ add_action('wp_enqueue_scripts', "add_my_css_and_my_js_files");
          
          <?php if($atts[lang] == "ro"): //Romanian Language ?>
          <?php setlocale(LC_ALL ,"ro_RO"); date_default_timezone_set('America/Los_Angeles'); 
-                      
-            if (file_exists(MY_PLUGIN_PATH.'xml/360-reading-plan-en.xml')) {
-                $xml = simplexml_load_file(MY_PLUGIN_PATH.'xml/360-reading-plan-ro.xml');
+             
+            //Check for XML, if exsit Include XML File
+            if (file_exists(bioy_path.'xml/360-reading-plan-en.xml')) {
+                $xml = simplexml_load_file(bioy_path.'xml/360-reading-plan-ro.xml');
 
             } else {
                 exit('Failed to open XML File.');
@@ -90,9 +81,7 @@ add_action('wp_enqueue_scripts', "add_my_css_and_my_js_files");
             $translation = $atts[translation];
 
          ?>
-            
-         
-         	<h2 class="bioy"><?php echo strftime("%B %Y"); ?></h2>
+          <h2 class="bioy"><?php echo strftime("%B %Y"); ?></h2>
           
             <ul class="bioy">
                 <?php foreach ($xml->children() as $verse): ?>
